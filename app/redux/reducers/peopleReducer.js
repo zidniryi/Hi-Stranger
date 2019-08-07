a662f26f2d6c923604971f40e1658ffb7db0e90e
zidniryi <zidnirydwan@gmail.com>
2019-08-07 22:26:35 +0700

    8 | create reducers and state global

--- app/redux/reducers/peopleReducer.js
+++ app/redux/reducers/peopleReducer.js
@@ -3,3 +3,25 @@ import {
     FETCHING_PEOPLE_SUCCESS, 
     FETCHING_PEOPLE_FAILURE }
      from '../actions/types';
+
+     const initialState = {
+         isFetching : false,
+         errorMessage:'',
+         people: []
+     
+     }
+     
+     export default (state = initialState, action) => {
+       switch (action.type) {
+     
+       case FETCHING_PEOPLE_REQUEST:
+         return { ...state, isFetching:true };
+      case FETCHING_PEOPLE_FAILURE:
+         return { ...screen, isFetching:false, errorMessage: action.payload}
+      case FETCHING_PEOPLE_SUCCESS:
+         return { ...state, isFetching:false, people: action.payload}     
+       default:
+         return state
+       }
+     };
+     
